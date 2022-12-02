77d7e81d1a09cac708d865e97a1f7fae4ea61122
DerRoobin <906113+derroobin@users.noreply.github.com>
2022-12-02 20:12:57 +0100

    prettier

--- src/days/d2.ts
+++ src/days/d2.ts
@@ -1,49 +1,49 @@
-import { qstring } from "../helper/readfile.js";
+import { qstring } from '../helper/readfile.js'
 
 const convert = (a: string) => {
-  if (a === "C" || a === "Z") return 0; // Scissor
-  if (a === "A" || a === "X") return 1; // Rock
-  if (a === "B" || a === "Y") return 2; // Paper
-};
+  if (a === 'C' || a === 'Z') return 0 // Scissor
+  if (a === 'A' || a === 'X') return 1 // Rock
+  if (a === 'B' || a === 'Y') return 2 // Paper
+}
 
 const part2 = (x: string) => {
-  const [a, b] = x.split(" ");
+  const [a, b] = x.split(' ')
   if (!a || !b) {
-    console.log("fehlerhaft", x);
-    return 0;
+    console.log('fehlerhaft', x)
+    return 0
   }
-  const y = convert(a);
-  const z = (convert(b) + 1 + y) % 3;
+  const y = convert(a)
+  const z = (convert(b) + 1 + y) % 3
 
-  const next = (y + 1) % 3;
+  const next = (y + 1) % 3
 
-  const winner = z === y ? 3 : next === z ? 6 : 0;
+  const winner = z === y ? 3 : next === z ? 6 : 0
 
-  return (z === 0 ? 3 : z) + winner;
-};
+  return (z === 0 ? 3 : z) + winner
+}
 
 const part1 = (x: string) => {
-  const [a, b] = x.split(" ");
+  const [a, b] = x.split(' ')
   if (!a || !b) {
-    return 0;
+    return 0
   }
-  const y = convert(a);
-  const z = convert(b);
+  const y = convert(a)
+  const z = convert(b)
 
-  const next = (y + 1) % 3;
+  const next = (y + 1) % 3
 
-  const winner = z === y ? 3 : next === z ? 6 : 0;
+  const winner = z === y ? 3 : next === z ? 6 : 0
 
-  return (z === 0 ? 3 : z) + winner;
-};
+  return (z === 0 ? 3 : z) + winner
+}
 
 export default async () => {
-  console.log(["A Y", "B X", "C Z"].map(part2));
+  console.log(['A Y', 'B X', 'C Z'].map(part2))
   const s = await (
-    await qstring("inputs/p2.txt")
+    await qstring('inputs/p2.txt')
   )
-    .split("\r\n")
+    .split('\r\n')
     .map(part1)
-    .reduce((p, c) => p + c, 0);
-  console.log(s);
-};
+    .reduce((p, c) => p + c, 0)
+  console.log(s)
+}
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,12 @@
-
-const days = new Array(24).fill(0).map((_, x) => x + 1);
-
 const main = async () => {
   try {
-    
-        const day = await (await import(`./days/d${(new Date).getDate()}.js`)).default
-        await day()
-      
-    
-
+    const day = await (
+      await import(`./days/d${new Date().getDate()}.js`)
+    ).default
+    await day()
   } catch (e) {
-    console.error(e);
+    console.error(e)
   }
-};
+}
 
-main();
+main()
