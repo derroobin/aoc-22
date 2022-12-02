748f2fa73551b9dabe9390311b8225c6e9326283
DerRoobin <906113+derroobin@users.noreply.github.com>
2022-12-02 20:13:21 +0100

    readfile prettier

--- src/helper/readfile.ts
+++ src/helper/readfile.ts
@@ -1,9 +1,8 @@
-import fs from "fs/promises"
+import fs from 'fs/promises'
 
 export const qfs = async (filepath: string) => {
   return fs.readFile(filepath)
-};
-
+}
 
 export const qstring = async (filepath: string) => {
   return (await qfs(filepath)).toString()
